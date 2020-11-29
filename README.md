458ff6cd2c424d37aaa5cd91cb0dcdc4ca0abc62
acshef <acshef@users.noreply.github.com>
2020-11-28 17:10:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Home Assistant Garage Door Keypad
+This project allows Home Assistant to host a publically exposed page allowing someone to enter a sequence of digits like those old RF garage door keypads.
+
+![Main UI](images/screenshot_main.png)
+
+## Setup
+1. Copy/merge the contents of the `/config` folder into your own. Additionally:
+    * This project requires [Home Assistant Packages](https://www.home-assistant.io/docs/configuration/packages/) turned on in `configuration.yaml`
+	* Update your `secrets.yaml` file to include an `garage_door_code_webhook_id` entry
+2. Update the automation in `/config/packages/garage_door_keypad.yaml`:
+    * Update the `notify.notify` service calls (or remove them if you don't want notifications)
+	* Update the garage-open service (do a Ctrl+F for "WEBHOOK_ACTION")
+3. Set the value of the `input_text.garage_door_code` entity in Home Assistant
+4. Create an NFC tag with a URL that points to:
+
+    ```
+    https://YOUR_HA_INSTANCE:PORT/local/garage_door_code/index.html?webhook_id=YOUR_GARAGE_DOOR_CODE_WEBHOOK_ID
+    ```
+
+5. And you're done! ✅
+
+## Notes/Use
+* The interface is mobile-specific &mdash; it's tested on Chrome/Android10, and Safari/iOS14
+* The interface features dark/light mode
+* In your Home Assistant instance, you can modify the keypad code via `input_text.garage_door_code`. If you set it to a blank value (empty string), the system will be "disabled" and no codes will work.
+* The `timer.until_garage_door_code_can_be_used` controls a server-side cooldown (so that nobody can rapidly brute-force the codes) &mdash; if you change its duration, make sure to change the `COOLDOWN` constant in `script.js`
+* Because the webhook ID is placed in the URL, it is **PUBLICALLY EXPOSED TO ANYONE THAT HAS ACCESS TO THE NFC TAG!** The automation doesn't allow you to perform any other actions with the webhook ID, but make sure you don't use it in place of other webhooks that expose other services.
---
+++ config/www/garage_door_code/script.js
@@ -0,0 +1,131 @@
+const displayField = document.getElementById("display")
+const digitButtons = Array.from(document.body.querySelectorAll(".digit"))
+const deleteButton = document.getElementById("del")
+const submitButton = document.getElementById("submit")
+const MAX_LENGTH = 10 // digits, should align with input_text in Home Assistant
+const params = new URLSearchParams(location.search)
+const WEBHOOK_ID = params.get("webhook_id")
+const COOLDOWN = 5000 // milliseconds, should align with cooldown timer in Home Assistant
+
+if (!WEBHOOK_ID) {
+	const errorMessage = 'URL is missing "webhook_id" parameter'
+	document.body.className += " disabled"
+	alert(errorMessage)
+	throw new Error(errorMessage) // Halt main thread
+}
+
+document.body.addEventListener("click", e => {
+	if (!e.target.classList.contains("digit")) {
+		return
+	}
+	displayField.value = displayField.value + e.target.innerText
+	vibrate(100)
+
+	if (displayField.value.length >= MAX_LENGTH) {
+		digitButtons.forEach(digit => (digit.disabled = true))
+	}
+	deleteButton.disabled = false
+	submitButton.disabled = false
+	submitButton.innerText = "Enter"
+})
+
+deleteButton.addEventListener("click", () => {
+	displayField.value = displayField.value.substring(0, displayField.value.length - 1)
+	digitButtons.forEach(digit => (digit.disabled = false))
+	vibrate(100)
+	if (displayField.value.length <= 0) {
+		deleteButton.disabled = true
+		submitButton.disabled = true
+	}
+	submitButton.innerText = "Enter"
+})
+let clearTimeoutID = undefined
+deleteButton.addEventListener("pointerdown", () => {
+	clearTimeout(clearTimeoutID)
+	clearTimeoutID = setTimeout(() => {
+		submitButton.disabled = true
+		digitButtons.forEach(digit => (digit.disabled = true))
+		deleteButton.disabled = true
+		submitButton.innerText = "Enter"
+		vibrate(displayField.value.length * 25)
+		const deleteDigitsIntervalID = setInterval(() => {
+			if (displayField.value.length) {
+				displayField.value = displayField.value.substring(0, displayField.value.length - 1)
+			} else {
+				clearInterval(deleteDigitsIntervalID)
+				digitButtons.forEach(digit => (digit.disabled = false))
+			}
+		}, 25)
+	}, 750)
+})
+deleteButton.addEventListener("pointerup", () => {
+	clearTimeout(clearTimeoutID)
+})
+
+submitButton.addEventListener("click", () => {
+	const code = displayField.value.trim()
+	if (!code.length) {
+		return
+	}
+	vibrate(100)
+	submitButton.disabled = true
+	deleteButton.disabled = true
+	digitButtons.forEach(digit => (digit.disabled = true))
+
+	submitButton.innerHTML = "<div class='spinner'></div>"
+
+	const promise = fetch(`/api/webhooks/${WEBHOOK_ID}`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({code: code}),
+	})
+
+	promise
+		.then(resp => {
+			if (!resp.ok) {
+				throw new Error("Bad network request")
+			}
+			const deleteDigitsIntervalID = setInterval(() => {
+				if (displayField.value.length) {
+					displayField.value = displayField.value.substring(
+						0,
+						displayField.value.length - 1
+					)
+				} else {
+					clearInterval(deleteDigitsIntervalID)
+					setTimeout(() => {
+						digitButtons.forEach(digit => (digit.disabled = false))
+						submitButton.innerText = "Enter"
+					}, COOLDOWN)
+				}
+			}, 50)
+		})
+		.catch(() => {
+			setTimeout(() => {
+				deleteButton.disabled = false
+				if (code.length < MAX_LENGTH) {
+					digitButtons.forEach(digit => (digit.disabled = false))
+				}
+				submitButton.disabled = false
+				submitButton.innerText = "Retry"
+				shake(submitButton)
+			}, COOLDOWN)
+		})
+})
+
+function shake(element) {
+	element.classList.add("shake")
+	setTimeout(() => {
+		element.classList.remove("shake")
+	}, 500)
+}
+
+function vibrate(duration) {
+	try {
+		navigator.vibrate(duration)
+	} catch (ex) {
+		// Silently fail
+	}
+}
